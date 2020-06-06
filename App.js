ded08e1904ec6e6c7db431351f38955acc264d8a
Lydiah <lydiah@3414.local>
2020-06-06 05:44:42 +0100

    Initial Commit

--- App.js
+++ App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let x=1;
+  console.log("App executed");
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native!!!</Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1,    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center', },
 });
